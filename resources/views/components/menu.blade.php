8da97afb9c7aa0fa61d31617be33c1fdcbb97fe9
Pouya Biglari <cyberman9000@gmail.com>
2022-03-08 16:49:44 +0330

    removed search from menu

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -14,7 +14,7 @@
                         <i class="fas fa-search fa-fw"></i>
                     </button>
                 </div>
-            </div><div class="sidebar-search-results"><div class="list-group"><a href="#" class="list-group-item"><div class="search-title"><strong class="text-light"></strong>N<strong class="text-light"></strong>o<strong class="text-light"></strong> <strong class="text-light"></strong>e<strong class="text-light"></strong>l<strong class="text-light"></strong>e<strong class="text-light"></strong>m<strong class="text-light"></strong>e<strong class="text-light"></strong>n<strong class="text-light"></strong>t<strong class="text-light"></strong> <strong class="text-light"></strong>f<strong class="text-light"></strong>o<strong class="text-light"></strong>u<strong class="text-light"></strong>n<strong class="text-light"></strong>d<strong class="text-light"></strong>!<strong class="text-light"></strong></div><div class="search-path"></div></a></div></div>
+            </div>
         </div>
 
         <nav class="mt-2">
