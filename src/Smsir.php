862aab21959d311bdcb71e9ef096cbab412bd20d
ahmadreza <ahmadreza@ahmadreza.ahmadreza>
2024-11-02 09:42:29 +0330

    Added doc blocks for method in Smsir facade

--- src/Smsir.php
+++ src/Smsir.php
@@ -5,6 +5,12 @@ namespace Cryptommer\Smsir;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method \Cryptommer\Smsir\Classes\Send Send()
+ * @method \Cryptommer\Smsir\Classes\Report Report()
+ * @method \Cryptommer\Smsir\Classes\Setting Setting()
+ * @method mixed post(string $path, array $params = [])
+ * @method mixed get(string $path, array $params = [])
+ * @method mixed delete(string $path, array $params = [])
  * @see \Cryptommer\Smsir\Skeleton\SkeletonClass
  */
 class Smsir extends Facade
